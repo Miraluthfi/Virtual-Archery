63044fe848431dd35426cf86e52b6f642359f3f0
AaL <63700874+Miraluthfi@users.noreply.github.com>
2023-04-09 13:46:55 +0700

    modify splash screen scene

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -4,39 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
-{
-    public GameObject menupanel;
-    public GameObject creditspanel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        menupanel.SetActive(true);
-        creditspanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
+{  
     public void PindahScene(string scenename)
     {
         SceneManager.LoadScene(scenename);
     }
-
-    public void CreditsButton()
-    {
-        menupanel.SetActive(false);
-        creditspanel.SetActive(true);
-    }
-
-    public void BackButton()
-    {
-        menupanel.SetActive(true);
-        creditspanel.SetActive(false);
-    }
-
     public void ExitButton()
     {
         Application.Quit();
