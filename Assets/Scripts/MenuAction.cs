267e13769d31066e8682eb8f43559116776f1ffe
AaL <63700874+Miraluthfi@users.noreply.github.com>
2023-04-13 23:13:52 +0700

    Add React

--- Assets/Scripts/MenuAction.cs
+++ Assets/Scripts/MenuAction.cs
@@ -10,16 +10,17 @@ using TMPro;
 public class MenuAction : MonoBehaviour
 {
     public GameObject panel, leave, cam;
-    public GameObject avatar;
+    public GameObject avatar, emoticon, react;
     public Transform[] spawn;
     public InputField txtInput;
-
-    private GameObject player;
+    
+    private GameObject player, emoji;
 
     // Start is called before the first frame update
     void Start()
     {
         leave.SetActive(false);
+        react.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,6 +43,7 @@ public class MenuAction : MonoBehaviour
         panel.SetActive(false);
         leave.SetActive(true);
         cam.SetActive(false);
+        react.SetActive(true);
 
         int i = Random.Range(0, spawn.Length);
         player = Instantiate(avatar, spawn[i].position, Quaternion.identity);
@@ -57,5 +59,18 @@ public class MenuAction : MonoBehaviour
         panel.SetActive(true);
         leave.SetActive(false);
         cam.SetActive(true);
+        react.SetActive(false);
+    }
+    public void React()
+    {
+        react = Instantiate(emoticon, player.transform.position + new Vector3(0, 0, 1), Quaternion.identity);
+
+        StartCoroutine(RemoveEmoticon());
+    }
+
+    IEnumerator RemoveEmoticon()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(react);
     }
 }
