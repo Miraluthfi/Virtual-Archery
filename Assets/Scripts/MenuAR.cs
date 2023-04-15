a115d96fb97ae8d494db8e3a4f41ee6aaf718d71
fahriangkat <mfahrialam12@gmail.com>
2023-04-15 22:18:25 +0700

    BARU

--- Assets/Scripts/MenuAR.cs
+++ Assets/Scripts/MenuAR.cs
@@ -5,33 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuAR : MonoBehaviour
 {
-    public GameObject PanelBody;
-    public GameObject Credits;
+    
 
-    private void Start()
-    {
-        PanelBody.SetActive(true);
-        Credits.SetActive(false);
-    }
+   
     public void PindahScene(string scenename)
     {
         SceneManager.LoadScene(scenename);
     }
-    public void ExitButton()
-    {
-        Application.Quit();
-        Debug.Log("Tombol Keluar telah di tekan");
-    }
-
-    public void BtnCredits()
-    {
-        PanelBody.SetActive(false);
-        Credits.SetActive(true);
-    }
-
-    public void BtnBack()
-    {
-        PanelBody.SetActive(true);
-        Credits.SetActive(false);
-    }
+   
 }
